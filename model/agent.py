f0f22bd60bc7207a25d4addc1549fa6fe478179e
kagari <e175766@ie.u-ryukyu.ac.jp>
2021-07-04 19:17:32 +0900

    word2vecで分散表現に変換する処理を追加

--- model/agent.py
+++ model/agent.py
@@ -1,5 +1,12 @@
 import os
 import MeCab
+from gensim.models.word2vec import Word2Vec
+
+arg = f'-Owakati -d {os.getenv("MECAB_CONFIG_DICDIR")}'
+tagger = MeCab.Tagger(arg)
+
+model_path = '/app/data/latest-ja-word2vec-gensim-model/word2vec.gensim.model'
+model = Word2Vec.load(model_path)
 
 class Agent():
     def __init__(self) -> None:
@@ -13,9 +20,16 @@ class Agent():
             str: システムの応答文
         """
         # 入力文を分かち書きにする
-        tagger = MeCab.Tagger(f'-Owakati -d {os.getenv("MECAB_CONFIG_DICDIR")}')
-        rep = tagger.parse(s)
+        words = tagger.parse(s).split(' ')
 
-        # TODO: Word2Vecで分散表現を得る
+        # Word2Vecで分散表現を得る
+        vec = []
+        for w in words:
+            try:
+                vec.append(model[w])
+            except KeyError:
+                if w != '':
+                    print(f'{w}')
+        rep = ','.join([str(v) for v in vec])
         
         return rep
