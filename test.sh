d02a60d306e3ff3878d837da06f9461b35d40a1d
kagari <e175766@ie.u-ryukyu.ac.jp>
2022-08-25 20:47:22 +0900

    [add] sample test file

---
+++ test.sh
@@ -0,0 +1,1 @@
+curl -XPOST http://10.100.0.40:18583/mattermost -H 'Content-Type:application/json' -d '{"token": "zjkkws1u93rzuy8xk4w3iz6ieo", "user_id": "aaaaaaa", "trigger_word": "!hey", "text": "!hey おはよう！"}'
