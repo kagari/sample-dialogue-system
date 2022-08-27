a4c1e48b4027fb5cccd47403bb069fc4c4090b47
kagari <e175766@ie.u-ryukyu.ac.jp>
2022-08-27 07:56:48 +0000

    [add] LINEResponderForEvaluation

--- routers/line.py
+++ routers/line.py
@@ -2,6 +2,7 @@ import configparser
 import logging
 from datetime import datetime
 from builtins import bytes
+import os
 import sys
 
 from fastapi import (
@@ -21,8 +22,8 @@ from linebot.models import (
 )
 from linebot.utils import PY3
 
-from model.agent import EchoAgent
-from controller import UserManager
+from model.agent import BaseAgent, EchoAgent
+from controller import DialogManager, UserManager
 
 
 router = APIRouter()
@@ -109,3 +110,35 @@ async def line_endpoint(request: Request, background_tasks: BackgroundTasks):
         stime, 'POST', '/line', signature, 200, "ok"
     )
     return "ok"
+
+
+class UserManagerForEval:
+    def __init__(self, agent1: BaseAgent, agent2: BaseAgent, eval_turn: int, max_utter_length: int, save_dir: str) -> None:
+        self.agent1 = agent1
+        self.agent2 = agent2
+        self.eval_turn = eval_turn
+        self.max_utter_length = max_utter_length
+        self.save_dir = save_dir
+        self.user_instances: dict[str, DialogManager] = dict()
+        self.latest_id = 0
+        if not os.path.isdir(self.save_dir):
+            os.makedirs(self.save_dir)
+        return
+
+    def reply(self, user_id: str, input_: str) -> str:
+        if user_id not in self.user_instances:
+            self.latest_id += 1
+            self.user_instances[user_id] = DialogManager(self.latest_id, self.agent1, self.save_dir, self.max_utter_length)
+        instance = self.user_instances[user_id]
+        reply = instance(input_)
+        if len(instance.dialog)//2 ==  self.eval_turn:
+            instance.dialog = []
+            # if instance.agent.name == self.agent1.name:
+            #     instance.agent = self.agent2
+            # else:
+            #     instance.agent = self.agent1
+        return reply
+
+
+class LINEResponderForEvaluation(UserManagerForEval):
+    pass
