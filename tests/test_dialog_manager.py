743d408b129995403649254a5e46d8de4d78ad29
kagari <e175766@ie.u-ryukyu.ac.jp>
2022-08-27 06:49:53 +0000

    [add] test line entrypoint

--- tests/test_dialog_manager.py
+++ tests/test_dialog_manager.py
@@ -22,6 +22,46 @@ class TestUserManager(unittest.TestCase):
             instance = user_manager.user_instances[user_id]
             self.assertEqual(instance.id, id_)
             self.assertEqual(instance.dialog, ['hello!', 'hello!'])
+        return
+
+
+class TestLINEResponderForEvaluation(unittest.TestCase):
+    """対話システムの評価用の変更内容をテストする。
+    具体的には以下の3つ。
+    n回の応答後に、
+    1. 対話履歴をリセットする
+    1. POST API（のモック）を叩いて Google Form の URL と 対話番号（{モデル名}-{ユーザid}）を送る
+    1. 対話モデルを入れ替える（BaseModel <-> GenderedModelなど）
+    """
+    def setUp(self) -> None:
+        self.eval_turn = 10
+        return
+
+    def tearDown(self) -> None:
+        return
+
+    def test_reset_dialog(self) -> None:
+        responder = LINEResponderForEvaluation(
+            agent=EchoAgent(),
+            eval_turn=self.eval_turn,
+            max_utter_length=1
+        )
+
+        for len_, input_ in enumerate(map(str, range(self.eval_turn-1))):
+            responder.entrypoint(request, background_task)
+            self.assertEqual(len(responder.manager.user_instances[request['user_id']].dialog), len_)
+            self.assertEqual(len(responder.manager.user_instances[request['user_id']].dialog[-1]), input_)
+        _ = responder.reply(str(self.eval_turn))
+        self.assertEqual(len(responder.manager.user_instances[request['user_id']].dialog), 0)
+        return
+
+    def test_post_form_url_and_dialog_id(self) -> None:
+        pass
+        return
+
+    def test_switch_dialog_model(self) -> None:
+        pass
+        return
 
 
 class TestResponseAPIforMattermost(unittest.TestCase):
